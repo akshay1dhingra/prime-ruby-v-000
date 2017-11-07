b4ab91fca28df12913367c9b2ebe90845a334616
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-11-07 18:02:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,32 +1,33 @@
-def primes(max_number)
+def sieve(max)
+  # Set up an array with all the numbers from 0 to the max
+  primes = (0..max).to_a
 
-  integers = []
-  i = 2
+  # Set both the first and second positions (i.e., 0 and 1) to nil, as they
+  # aren't prime.
+  primes[0] = primes[1] = nil
+  
+  # Iterate through primes array
+  counter = 0
+  primes.each do |p|
+    # Skip if nil
+    next unless p
 
-  while i <= max_number
-    integers << i
-    i = i + 1
+    # Break if we are past the square root of the max value 
+    break if p*p > max
+    counter += 1
+    # Start at the square of the current number, and step through.
+    # Go up to the max value, by multiples of the current number, and replace
+    # that value with nil in the primes array
+    (p*p).step(max,p) { |m| primes[m] = nil }
   end
 
-  index = 0
-  multiplier = 2
-
-  while integers[index] != nil
-    while (integers[index] * multiplier) <= max_number
-      integers.delete(integers[index] * multiplier)
-      multiplier = multiplier + 1
-    end
-
-    index = index + 1
-    multiplier = 2
-  end
-
-  p integers
-
+  # Finally, return the compacted array.
+  puts "Solved for #{max} in #{counter} steps."
+  primes.compact
 end
 
 def prime?(num)
-  primes(num).include?(num)
+  sieve(num).include?(num)
 end
 
-puts prime?(100)
+puts prime?(105557)
