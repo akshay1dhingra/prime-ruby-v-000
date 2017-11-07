da15458950572a7b650c4c71328587c84b75a50d
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-11-07 16:53:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,6 +8,14 @@ def primes(max_number)
     i = i + 1
   end
 
+  index = 0 
+  multiplier = 2
+
+  while (integers[index] * multiplier) <= max_number
+    integers.delete(integers[index] * multiplier)
+    multiplier = multiplier + 1
+  end
+ 
   p integers
 
 end
